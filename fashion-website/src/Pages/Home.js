1c518cf9b94aea8ad1dcc004cd1feacab4c59d29
Rajamanthri <mcrajamanthri50@gmail.com>
2024-02-21 14:07:49 +0530

    do some changes to the navigationbar

---
+++ fashion-website/src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navigationbar from '../components/Navigationbar'
+
+export default function Home() {
+  return (
+    <div>
+      <Navigationbar/>
+    </div>
+  )
+}
