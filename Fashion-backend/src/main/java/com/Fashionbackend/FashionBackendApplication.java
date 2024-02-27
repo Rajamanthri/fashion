4eb48403d9aeca405f8f67c3d773c7832a5dbc90
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-02-28 00:23:14 +0530

    Backend
    This is the Backend of the website

---
+++ Fashion-backend/src/main/java/com/Fashionbackend/FashionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Fashionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionBackendApplication.class, args);
+	}
+
+}
