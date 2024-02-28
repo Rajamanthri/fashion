1baaf8015b7eeec9160760626e949b41274bd96d
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-02-28 23:23:00 +0530

    Update Users.java

--- Fashion-backend/src/main/java/com/Fashionbackend/Entity/Users.java
+++ Fashion-backend/src/main/java/com/Fashionbackend/Entity/Users.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+<<<<<<< Updated upstream:Fashion-backend/src/main/java/com/Fashionbackend/Entity/Users.java
 public class Users {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +21,14 @@ public class Users {
     private  int contact;
     private int role;
     private String Address;
+=======
+public class Order {
+    @Id
+    
+    private long oid;
+    private long pid;
+    private String color;
+    private String size;
+    private int quantity;
+>>>>>>> Stashed changes:Fashion-backend/src/main/java/com/Fashionbackend/Entity/Order.java
 }
