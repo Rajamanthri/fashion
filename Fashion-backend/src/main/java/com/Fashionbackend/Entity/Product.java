3cc01e9f90b05ea82a9ac2f301d508698dec643e
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-02-29 12:38:55 +0530

    successfull created crud operation in product table

--- Fashion-backend/src/main/java/com/Fashionbackend/Entity/Product.java
+++ Fashion-backend/src/main/java/com/Fashionbackend/Entity/Product.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 =======
 import jakarta.persistence.*;
->>>>>>> Stashed changes
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
