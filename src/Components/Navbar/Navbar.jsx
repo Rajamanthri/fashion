d335c6aafc7bc21e3ef8f062a2dedd9857483e54
sewmini25 <sewminidesilva4@gmail.com>
2024-03-03 17:09:06 -0800

    Create search bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,41 +7,43 @@ import { faHeart, faShoppingCart, faSearch } from '@fortawesome/free-solid-svg-i
 
 
 const Navbar = () => {
-  
-   let role = localStorage.getItem("authToken");
+
+  let role = localStorage.getItem("authToken");
   return (
     <nav className="navbar">
       <div className="nav-logo">
         <Link to="/">Wow Fashion</Link>
       </div>
       <div className="nav-search">
-        <input type="text" placeholder="Search" />
-        <button type="button"><FontAwesomeIcon icon={faSearch} /></button>
+        <form onSubmit={handleSearchSubmit}> {/* Attach handleSearchSubmit function to form onSubmit event */}
+          <input type="text" placeholder="Search" value={searchTerm} onChange={handleSearchInputChange} />
+          <button type="submit"><FontAwesomeIcon icon={faSearch} /></button> {/* Use submit button to trigger form submission */}
+        </form>
       </div>
       <ul className="nav-menu">
-      <li><Link to="/kids">ALL</Link></li>
+        <li><Link to="/kids">ALL</Link></li>
         <li><Link to="/men">Frock</Link></li>
         <li><Link to="/women">Sarees</Link></li>
         {role === "admin" ? (
-  <li><Link to="/women">Addproduct</Link></li>
-) : (
-  null
-)}
-        
+          <li><Link to="/women">Addproduct</Link></li>
+        ) : (
+          null
+        )}
+
       </ul>
 
       <div className="nav-actions">
-       
+
         <div className="cart-icon-container">
-            <div className="cart"><Link to='/cart'><FontAwesomeIcon icon={faShoppingCart} /></Link></div>
-            <div className="nav-cart-count">0</div>
-          </div>
-        
-        
+          <div className="cart"><Link to='/cart'><FontAwesomeIcon icon={faShoppingCart} /></Link></div>
+          <div className="nav-cart-count">0</div>
+        </div>
+
+
 
         <div className="nav-login">
           <Link to='/login'><button type="button" class="login">Login</button></Link>
-          
+
         </div>
       </div>
     </nav>
